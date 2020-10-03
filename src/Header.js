0e4e9fee7614406ff19cdb5230babb6baf32c295
Ibtid <ibtid011@example.com>
2020-10-04 02:39:48 +0600

    material ui icon

---
+++ src/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img
+        className="header__logo"
+        src="https://images-na.ssl-images-amazon.com/images/G/01/gc/designs/livepreview/amazon_dkblue_noto_email_v2016_us-main._CB468775337_.png"
+        alt="AMAZON"
+      />
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Order</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCO5u_j6PkS2xLA3OJG5IMfIUFvF1WIQfo",
+  authDomain: "e-clone-596cb.firebaseapp.com",
+  databaseURL: "https://e-clone-596cb.firebaseio.com",
+  projectId: "e-clone-596cb",
+  storageBucket: "e-clone-596cb.appspot.com",
+  messagingSenderId: "842891239375",
+  appId: "1:842891239375:web:f44ff13d7df98cdb913cba",
+  measurementId: "G-ZFEZM317XC",
+};
