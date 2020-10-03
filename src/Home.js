9783e313aa7dc51357b039c66f5d0a523d4b3353
Ibtid <ibtid011@example.com>
2020-10-04 04:04:38 +0600

    banner

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Home.css";
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/uk-certifiedrefurbished/renewed/UK_AR_Storebanner_Jul19.jpg"
+          alt="Banner"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
