8093b8e228233c3a0ecc6a6a45ae058133140a64
Ibtid <ibtid011@example.com>
2020-10-04 06:41:02 +0600

    Checkout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
 import Header from "./Header";
 import Home from "./Home";
+import Checkout from "./Checkout";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    //BEM
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
