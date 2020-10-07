81d83202b0ca88ac1bbeff08c1a3f9851e146aed
Ibtid <ibtid011@example.com>
2020-10-07 11:49:40 +0600

    Updates

---
+++ precache-manifest.bd2ab32a2bccce1f2a0c0b0746ae5eb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcf926413ad80977fc0076ac703a6523",
+    "url": "/Amazon-clone/index.html"
+  },
+  {
+    "revision": "50e1a9fcb8946423d182",
+    "url": "/Amazon-clone/static/css/main.7b67509f.chunk.css"
+  },
+  {
+    "revision": "e36d842d500530de4c1d",
+    "url": "/Amazon-clone/static/js/2.e4a94089.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/Amazon-clone/static/js/2.e4a94089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e1a9fcb8946423d182",
+    "url": "/Amazon-clone/static/js/main.9818b9fa.chunk.js"
+  },
+  {
+    "revision": "5a250a1a481434fdae88",
+    "url": "/Amazon-clone/static/js/runtime-main.e640481e.js"
+  }
+]);
